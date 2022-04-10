71bf15928087ae5de525d95fd9f2291bcedecac5
mjste <mj.stefanik@gmail.com>
2022-04-11 00:02:48 +0200

    hopefully near the end

--- cw05/zad1/main.c
+++ cw05/zad1/main.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
     // save components to array
     char components[MAX_VARIABLE_COUNT][BUFFER_SIZE];
     int index = 0;
-    // int complen = 0;
+    int complen = 0;
     while (1)
     {
         if (index >= MAX_VARIABLE_COUNT)
@@ -41,7 +41,16 @@ int main(int argc, char **argv)
 
         index++;
     }
-    // complen = index;
+    complen = index;
+
+    // process components
+    for (int i = 0; i < complen; i++)
+    {
+        char *arg;
+        arg = strtok(components[i], "=");
+        arg = strtok(NULL, "\n");
+        strcpy(components[i], arg + 1);
+    }
 
     // save commands to array
     char commands[MAX_VARIABLE_COUNT][BUFFER_SIZE];
@@ -84,8 +93,8 @@ void function(int index, char components[MAX_VARIABLE_COUNT][BUFFER_SIZE], char 
     char *arg;
     arg = strtok(commands[index], " ");
 
+    // create component list
     arg += 8;
-    // printf("%s\n", arg);
     cmds[0] = atoi(arg);
     int cmdslen = 1;
     while ((arg = strtok(NULL, " ")))
@@ -96,15 +105,79 @@ void function(int index, char components[MAX_VARIABLE_COUNT][BUFFER_SIZE], char 
         if (arg[0] != '|')
         {
             arg += 8;
-            // printf("%s\n", arg);
             cmds[cmdslen++] = atoi(arg);
         }
     }
+    // Done
 
-    for (int j = 0; j < cmdslen; j++)
+    // create one command
+    char cmd[BUFFER_SIZE];
+    cmd[0] = 0;
+    for (int i = 0; i < cmdslen; i++)
     {
-        printf("%d ", cmds[j]);
+        strcat(cmd, components[cmds[i] - 1]);
+        strcat(cmd, " | ");
+    }
+    // null last space
+    cmd[strlen(cmd) - 1] = 0;
+    printf("One command: %s\n", cmd);
+
+    // put command into subcommands
+    char subcommands[MAX_VARIABLE_COUNT][BUFFER_SIZE];
+    char *cmdn = cmd;
+    arg = NULL;
+    int subcmdn = 0;
+    while ((arg = strtok(cmdn, "|")))
+    {
+        cmdn = NULL;
+        if (arg[0] == ' ')
+            arg++;
+        strcpy(subcommands[subcmdn++], arg);
+        // printf("%s\n", arg);
     }
 
-    printf("\n");
+    printf("subcommands:\n");
+    for (int i = 0; i < subcmdn; i++)
+    {
+        printf("%s\tlen: %ld\n", subcommands[i], strlen(subcommands[i]));
+    }
+    printf("\n\n");
+
+    // create pipes
+    int *pipes = calloc((subcmdn - 1) * 2, sizeof(int));
+    for (int i = 0; i < subcmdn - 1; i++)
+    {
+        pipe(pipes + 2 * i);
+    }
+
+    for (int i = 0; i < subcmdn; i++)
+    {
+        if (fork() == 0)
+        {
+            if (i == 0)
+            {
+                dup2(STDOUT_FILENO, pipes[1]);
+            }
+            else if (i == subcmdn - 1)
+            {
+                dup2(2 * subcmdn - 4, STDIN_FILENO);
+            }
+            else
+            {
+                dup2(pipes[2 * (i - 1)], STDIN_FILENO);
+                dup2(STDOUT_FILENO, pipes[2 * i + 1]);
+            }
+
+            exit(0);
+        }
+    }
+    for (int i = 0; i < 2 * (subcmdn - 1); i++)
+    {
+        close(pipes[i]);
+    }
+
+    for (int i = 0; i < subcmdn; i++)
+        wait(NULL);
+
+    free(pipes);
 }
