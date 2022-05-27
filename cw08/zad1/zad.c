9b27c6682b6093fd1baae732826d67e5434c77da
mjste <mj.stefanik@gmail.com>
2022-05-27 10:57:56 +0200

    need to add more precise time measurment

--- cw08/zad1/zad.c
+++ cw08/zad1/zad.c
@@ -3,29 +3,41 @@
 #include <limits.h>
 #include <ctype.h>
 #include <pthread.h>
+#include <string.h>
+#include <sys/times.h>
+#include <unistd.h>
 
 enum p_variant
 {
-    numbers,
-    block
+    NUMBERS,
+    BLOCK
 };
 
 struct image
 {
     int rows;
     int cols;
-    char **array;
+    unsigned char **array;
+};
+
+struct thread_argument
+{
+    struct image *img_in;
+    struct image *img_out;
+    enum p_variant variant;
+    int from;
+    int to;
 };
 
 void process_arguments(char **argv, int *no_of_threads, int *p_var);
-void thread_function(void *p);
+void *thread_handler(void *args);
 
 int main(int argc, char **argv)
 {
     /*
     arguments:
     number of threads
-    type: numbers / block
+    type: NUMBERS / BLOCK
     fin
     fout
     */
@@ -66,22 +78,58 @@ int main(int argc, char **argv)
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < cols; j++)
             fscanf(fin, "%d", (int *)&img1.array[i][j]);
+    // close fin
+    fclose(fin);
 
     // create_arguments
-    int args[20]; // mock, nie korzystać
+    int range_min = 0;
+    int range_max;
+    switch (program_variant)
+    {
+    case NUMBERS:
+        range_max = 256;
+        break;
+    case BLOCK:
+        range_max = rows * cols;
+        break;
+    }
+
+    struct thread_argument **args = malloc(number_of_threads * sizeof(struct thread_argument *));
+    for (int i = 0; i < number_of_threads; i++)
+    {
+        args[i] = malloc(sizeof(struct thread_argument));
+        args[i]->img_in = &img1;
+        args[i]->img_out = &img2;
+        args[i]->variant = program_variant;
+        args[i]->from = range_min + (range_max - range_min) * i / number_of_threads;
+        args[i]->to = range_min + (range_max - range_min) * (i + 1) / number_of_threads;
+    }
 
     // create threads
     pthread_t *threadsp = malloc(number_of_threads * sizeof(pthread_t));
     for (int i = 0; i < number_of_threads; i++)
     {
-        if (pthread_create(&threadsp[i], NULL, thread_function, args[i]))
-            ;
+        if (pthread_create(&threadsp[i], NULL, &thread_handler, args[i]) != 0)
+        {
+            perror("Creating threads");
+            exit(0);
+        }
     }
 
-    // reverse colors
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-            img2.array[i][j] = 255 - img1.array[i][j];
+    // wait for threads
+    double *time_arr = malloc(number_of_threads * sizeof(double));
+    for (int i = 0; i < number_of_threads; i++)
+    {
+        double *timep;
+        pthread_join(threadsp[i], (void **)&timep);
+        time_arr[i] = *timep;
+    }
+    puts("Threads finished");
+
+    for (int i = 0; i < number_of_threads; i++)
+    {
+        printf("%f \n", time_arr[i]);
+    }
 
     // save image
     fprintf(fout, "%s\n%d %d\n%d\n", img_mode, rows, cols, max_val);
@@ -103,7 +151,6 @@ int main(int argc, char **argv)
     free(img1.array);
     free(img2.array);
 
-    fclose(fin);
     fclose(fout);
 
     return 0;
@@ -114,7 +161,7 @@ void process_arguments(char **argv, int *no_of_threads, int *p_var)
     /*
     arguments:
     number of threads
-    type: numbers / block
+    type: NUMBERS / BLOCK
     fin
     fout
     */
@@ -127,16 +174,18 @@ void process_arguments(char **argv, int *no_of_threads, int *p_var)
     }
     *no_of_threads = atoi(argv[1]);
 
-    // p_var = {0|1}
-    if (!isdigit(argv[2][0]))
+    // p_var
+    if (strcmp(argv[2], "numbers") == 0)
     {
-        fprintf(stderr, "Second argument is not a number\n");
-        exit(-1);
+        *p_var = NUMBERS;
     }
-    *p_var = atoi(argv[2]);
-    if (*p_var > 1 || *p_var < 0)
+    else if (strcmp(argv[2], "block") == 0)
     {
-        fprintf(stderr, "error: bad mode, try 0 or 1\n");
+        *p_var = BLOCK;
+    }
+    else
+    {
+        fprintf(stderr, "error: bad mode, try 'numbers' or 'block'\n");
         exit(-1);
     }
 
@@ -160,4 +209,41 @@ void process_arguments(char **argv, int *no_of_threads, int *p_var)
     return;
 }
 
-void thread_function(void *p);
+void *thread_handler(void *args)
+{
+    time_t start = times(NULL);
+    time_t stop;
+    struct thread_argument *arg = args;
+    switch (arg->variant)
+    {
+    case NUMBERS:
+        for (int i = 0; i < arg->img_in->rows; i++)
+        {
+            for (int j = 0; j < arg->img_in->cols; j++)
+            {
+                unsigned char value = arg->img_in->array[i][j];
+                if (value >= arg->from && value < arg->to)
+                {
+                    arg->img_out->array[i][j] = 255 - value;
+                }
+            }
+        }
+        break;
+    case BLOCK:
+    {
+        int i = arg->from;
+        while (i < arg->to)
+        {
+            int row = i / arg->img_in->cols;
+            int col = i % arg->img_in->cols;
+            arg->img_out->array[row][col] = 255 - arg->img_in->array[row][col];
+            i++;
+        }
+        break;
+    }
+    }
+    stop = times(NULL);
+    double *running_time = malloc(sizeof(double));
+    *running_time = ((double)stop - (double)start) / (double)sysconf(_SC_CLK_TCK);
+    return running_time;
+}
